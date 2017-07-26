2b1239b8bdb3fc48d05dadc779f7fe7769dcdce3
Johannes Castner <jcastner@ebay.com>
2017-07-26 21:23:37 +0000

    some simple experiments with functions

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,4 +1,11 @@
-object HelloWorld2 extends Application {
-  println("Hello, world!")
+object Experiments extends Application {
+  def encode(n: Int, f: (Int) => Long): Long = {
+    val x = n * 10
+    f(x)
+  }
+  println("encode(100, (x: Int)=> x + 100): Long")
+  println(encode(100, (x: Int) => x + 100): Long)
+  println("encode(100, (x: Int)=> x*5): Long")
+  println(encode(100, (x: Int)=> x*5): Long)
 
 }
